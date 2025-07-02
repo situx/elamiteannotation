c9b2d7e1b4b8ae49d1c549a0add0418e79764e97
Timo Homburg <situx@users.noreply.github.com>
2025-07-02 22:56:03 +0200

    stats

---
+++ js/data/stats.js
@@ -0,0 +1,30 @@
+var stats={
+  "itemsWithQID": 2207,
+  "itemWithSenseDE": 4665,
+  "wordcase": {
+    "noun": 4523,
+    "GN": 1802,
+    "PN": 6068,
+    "verb": 1833,
+    "adjective": 115,
+    "postposition": 7,
+    "adverb": 43,
+    "unknown": 1,
+    "DN": 344,
+    "noun, adverb": 1,
+    "adveb": 1,
+    "conjunction": 1,
+    "other": 770,
+    "demonstrative pronoun": 4,
+    "pronoun": 1
+  },
+  "period": {
+    "achE": 8209,
+    "nE": 1981,
+    "mE": 1661,
+    "oE": 2386
+  },
+  "completereference": 6208,
+  "done": 542,
+  "itemcount": 15571
+}
